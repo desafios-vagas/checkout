fa33c312ed2c89ca4cd9be81a9a668681d53f5fc
eliasfernandescout <eliasfernandescout@gmail.com>
2024-10-13 16:08:37 -0300

    iniciando kafka

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CustomerModule } from './customer/customer.module';
@@ -12,6 +13,20 @@ import { OrderModule } from './order/order.module';
     MongooseModule.forRoot('mongodb://mongo:mongo123@3.129.59.213:27017/checkout', {
       authSource: 'admin',
     }),
+    ClientsModule.register([
+      {
+        name: 'KAFKA_SERVICE',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            brokers: ['3.129.59.213:9092'],
+          },
+          consumer: {
+            groupId: 'my-consumer-' + Math.random(),
+          },
+        },
+      },
+    ]),
     CustomerModule,
     CreditCardModule,
     ProductModule,
